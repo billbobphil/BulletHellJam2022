f45e6e7f41bc25b92f8e6c5c12f1fd9d6050e554
billbobphil <37590467+billbobphil@users.noreply.github.com>
2022-04-15 15:00:21 -0700

    Sword position now tracks mouse - bug present when mouse is over sword or player character

---
+++ Assets/Scripts/SwordPositionController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordPositionController : MonoBehaviour
+{
+    private Camera _mainCamera;
+    
+    private void Start()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    private void FixedUpdate()
+    {
+        AimSwordAtMouse();
+    }
+
+    private void AimSwordAtMouse()
+    {
+        Vector3 mousePosition = _mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        PointSwordAtTarget(mousePosition, gameObject.transform.position, gameObject, 1);
+    }
+
+    private static void PointSwordAtTarget(Vector2 targetPosition, Vector2 shooterPosition, GameObject sword, float distanceBetweenPlayerAndSword)
+    {
+        double x = targetPosition.x - shooterPosition.x;
+        double y = targetPosition.y - shooterPosition.y;
+        
+        double angleInRadians = Math.Atan2(x, y);
+        double angleInDegrees = angleInRadians * (180 / Mathf.PI);
+        
+        double smallX = distanceBetweenPlayerAndSword * Math.Sin(angleInRadians);
+        double smallY = distanceBetweenPlayerAndSword * Math.Cos(angleInRadians);
+        
+        Vector3 newSwordPosition = new Vector3((float)smallX, (float)smallY, 0);
+        
+        Vector3 newSwordAngle = new Vector3(0, 0, (float)-angleInDegrees);
+        
+        sword.transform.localPosition = newSwordPosition;
+        sword.transform.eulerAngles = newSwordAngle;
+    }
+}
