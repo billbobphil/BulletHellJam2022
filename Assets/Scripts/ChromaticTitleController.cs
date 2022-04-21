46fef2c60e1f6d69d29da67d5f28e1aa41864707
billbobphil <37590467+billbobphil@users.noreply.github.com>
2022-04-20 20:14:34 -0700

    Adjusted some overly loud sound effects. Added a random wave effect to the CHROMATIC title on the title card.

--- Assets/Scripts/ChromaticTitleController.cs
+++ Assets/Scripts/ChromaticTitleController.cs
@@ -1,18 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ChromaticTitleController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private List<float> _startingPositions = new List<float>();
+    private List<float> _directions = new List<float>();
+    private List<float> _speeds = new List<float>();
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        foreach (Transform child in transform)
+        {
+            _startingPositions.Add(child.position.y);
+
+            int random = Random.Range(1, 10);
+
+            int startingDirection = random > 5 ? 1 : -1;
+            
+            _directions.Add(startingDirection);
+
+            float speed = Random.Range(0.1f, 0.3f);
+            _speeds.Add(speed);
+        }
+    }
+    
+    private void FixedUpdate()
+    {
+        int i = 0;
+        foreach (Transform child in transform)
+        {
+            if (child.position.y >= _startingPositions[i] + 10f ||
+                child.position.y <= _startingPositions[i] - 10f)
+            {
+                _directions[i] = _directions[i] *= -1;
+            }
+            
+            child.Translate(new Vector3(0, _directions[i] * _speeds[i], 0));
+            // child.Translate(new Vector3(0, 1, 0));
+
+            i++;
+        }
     }
 }
